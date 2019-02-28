ab68d90d6eace88542686081d020da0a4cb74b37
SultanAmmar <m.sultanammar@gmail.com>
2019-02-28 15:44:18 +0100

    Create first class MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,13 @@
 package com.example.myapplication;
 
 public class MainActivity {
+    private int i;
+    private float j;
+    private String s;
+
+    public MainActivity(int i, float j, String s){
+        i = i;
+        j = j;
+        s = s;
+    }
 }
