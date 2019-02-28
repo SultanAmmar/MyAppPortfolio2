fece1560d563af5d69c7137be6db2ed280fc9985
SultanAmmar <m.sultanammar@gmail.com>
2019-02-28 15:53:45 +0100

    Create FeatureActivity

---
+++ app/src/main/java/com/example/myapplication/FeatureActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+public class FeatureActivity {
+    private int a;
+    private float b;
+    private String c;
+
+    public FeatureActivity(int a, float b, String c){
+        //constructor
+        a = a;
+        b = b;
+        c = c;
+    }
+}
