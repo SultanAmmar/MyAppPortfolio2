f696db67c8bc4331037a1790f86116c633004df5
SultanAmmar <m.sultanammar@gmail.com>
2019-04-08 11:43:22 +0100

    Bug fix : NullPointerException

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,18 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
     private int i;
     private float j;
     private String s;
 
-    private Button btn1;
-    private Button btn2;
+    Button btn1;
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         i = i;
         j = j;
         s = s;
@@ -26,22 +26,22 @@ public class MainActivity extends Activity {
         btn1 = (Button)findViewById(R.id.activity_main_btn1);
         btn2 = (Button)findViewById(R.id.activity_main_btn2);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            /*
-            الحمد ببه
-            Today, I created two buttons for test.
-             */
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Button 1 Clicked ! ", Toast.LENGTH_SHORT).show();
-            }
-        });
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Button 2 Clicked !",Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.activity_main_btn1:
+                Toast.makeText(getApplicationContext(),"Button 1 Clicked !",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.activity_main_btn2:
+                Toast.makeText(getApplicationContext(),"Button 2 Clicked !", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
